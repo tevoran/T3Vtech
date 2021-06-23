af2a02da2a8d1f29ec2f9835d333d061a4a9baf8
tevoran <j.pfeiffer93@gmx.de>
2021-06-23 16:09:30 +0200

    test scene is now ow more beautiful

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,9 @@ int main()
 	t3v::engine& te=t3v::engine::get();
 	
 	//setting properties
-	te.set_resx(1366);
-	te.set_resy(768);
-	te.set_fullscreen(false);
+	te.set_resx(1920);
+	te.set_resy(1080);
+	te.set_fullscreen(true);
 
 	te.start_renderer(TE_RENDERER_SOFTWARE_RASTERIZER);
 
@@ -24,21 +24,28 @@ int main()
 	box.position({-2.5,0,4});
 	box.use_texture(crate);
 
-	t3v::texture *road_tex=t3v::load_texture("../assets/road.jpg");
+	t3v::texture *road_tex=t3v::load_texture("../assets/road.png");
 	t3v::object3d ground;
 	ground.make_quad(20, 20);
 	ground.position({0,-1.5,10.1});
 	ground.rotate({1,0,0}, 90);
 	ground.use_texture(road_tex);
 
+	t3v::texture *sky_tex=t3v::load_texture("../assets/Sky-072.jpg");
+	t3v::object3d sky;
+	sky.make_quad(160, 90);
+	sky.position({0,40,20});
+	sky.use_texture(sky_tex);
+
 	te.activate_fps_counter(true, &font);
 	for(int i=0; i<FRAMES; i++)
 	{
 		box.render();
 		ground.render();
+		sky.render();
 
-		
-		te.print_single_frame("T3Vtech - software renderer", font, {10,240,10, 255}, 32, te.get_resx()-430, te.get_resy()-48);
+		std::string text="T3Vtech - software renderer";
+		te.print_single_frame(text, font, {10,240,10, 255}, 32, te.get_resx()-430, te.get_resy()-48);
 
 		te.update();
 	}
