9f3cca8d4777cf862ac32bdc56cdc1371a1723a8
tevoran <j.pfeiffer93@gmx.de>
2021-08-06 12:03:58 +0200

    the Windows version of getting the number of available CPU threads

--- v2/src/engine/misc/multithreading.c
+++ v2/src/engine/misc/multithreading.c
@@ -6,11 +6,20 @@
 	#include <sys/sysinfo.h>
 #endif
 
-//linux solution for number of CPU threads
-#ifdef __linux__
+#ifdef PTW32_VERSION
+	#include <pthread.h>
+#endif
 
+//linux solution for the number of CPU threads
+#ifdef __linux__
 int te_num_procs() {
 	return get_nprocs();
 }
+#endif
 
+//Windows POSIX version for the number of CPU threads
+#ifdef PTW32_VERSION
+int te_num_procs() {
+    return pthread_num_processors_np();
+}
 #endif
