fab42a522a46a1a47f2337e496163fd8e639c04b
Jeffrey Skinner <Jeff@Pyusoft.com>
2021-08-14 22:21:22 -0700

    Fixed te_swap and began importing new rendering functions

---
+++ v2/build/CMakeFiles/T3Vtech.dir/cmake_clean.cmake
@@ -0,0 +1,39 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/T3Vtech.dir/src/arch/amd64/linux/multithreading.c.o"
+  "CMakeFiles/T3Vtech.dir/src/arch/amd64/linux/multithreading.c.o.d"
+  "CMakeFiles/T3Vtech.dir/src/arch/amd64/windows/multithreading.c.o"
+  "CMakeFiles/T3Vtech.dir/src/arch/amd64/windows/multithreading.c.o.d"
+  "CMakeFiles/T3Vtech.dir/src/engine/general/general.c.o"
+  "CMakeFiles/T3Vtech.dir/src/engine/general/general.c.o.d"
+  "CMakeFiles/T3Vtech.dir/src/engine/general/init.c.o"
+  "CMakeFiles/T3Vtech.dir/src/engine/general/init.c.o.d"
+  "CMakeFiles/T3Vtech.dir/src/engine/general/multithreading.c.o"
+  "CMakeFiles/T3Vtech.dir/src/engine/general/multithreading.c.o.d"
+  "CMakeFiles/T3Vtech.dir/src/engine/math/barycentric_interpolation.c.o"
+  "CMakeFiles/T3Vtech.dir/src/engine/math/barycentric_interpolation.c.o.d"
+  "CMakeFiles/T3Vtech.dir/src/engine/misc/misc.c.o"
+  "CMakeFiles/T3Vtech.dir/src/engine/misc/misc.c.o.d"
+  "CMakeFiles/T3Vtech.dir/src/engine/renderer/renderer.c.o"
+  "CMakeFiles/T3Vtech.dir/src/engine/renderer/renderer.c.o.d"
+  "CMakeFiles/T3Vtech.dir/src/engine/renderer/software_rasterizer/init.c.o"
+  "CMakeFiles/T3Vtech.dir/src/engine/renderer/software_rasterizer/init.c.o.d"
+  "CMakeFiles/T3Vtech.dir/src/engine/renderer/software_rasterizer/quit.c.o"
+  "CMakeFiles/T3Vtech.dir/src/engine/renderer/software_rasterizer/quit.c.o.d"
+  "CMakeFiles/T3Vtech.dir/src/engine/renderer/software_rasterizer/rasterize_triangle.c.o"
+  "CMakeFiles/T3Vtech.dir/src/engine/renderer/software_rasterizer/rasterize_triangle.c.o.d"
+  "CMakeFiles/T3Vtech.dir/src/engine/renderer/software_rasterizer/render_call.c.o"
+  "CMakeFiles/T3Vtech.dir/src/engine/renderer/software_rasterizer/render_call.c.o.d"
+  "CMakeFiles/T3Vtech.dir/src/engine/renderer/software_rasterizer/render_thread.c.o"
+  "CMakeFiles/T3Vtech.dir/src/engine/renderer/software_rasterizer/render_thread.c.o.d"
+  "CMakeFiles/T3Vtech.dir/src/engine/renderer/software_rasterizer/update.c.o"
+  "CMakeFiles/T3Vtech.dir/src/engine/renderer/software_rasterizer/update.c.o.d"
+  "CMakeFiles/T3Vtech.dir/src/main.c.o"
+  "CMakeFiles/T3Vtech.dir/src/main.c.o.d"
+  "T3Vtech"
+  "T3Vtech.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/T3Vtech.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ v2/build/CMakeFiles/T3Vtech.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for T3Vtech.
+# This may be replaced when dependencies are built.
